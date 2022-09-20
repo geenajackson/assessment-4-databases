70c20c9f4a3b07e4fb5aa4dde652635e14508d83
Geena Jackson <geena.jackson96@gmail.com>
2022-09-20 09:52:47 -0500

    create models for playlists, songs, and playlists_songs

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -8,19 +8,31 @@ db = SQLAlchemy()
 class Playlist(db.Model):
     """Playlist."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text)
 
 
 class Song(db.Model):
     """Song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "songs"
 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    artist = db.Column(db.String, nullable=False)
 
 class PlaylistSong(db.Model):
     """Mapping of a playlist to a song."""
 
-    # ADD THE NECESSARY CODE HERE
+    __tablename__ = "playlists_songs"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlists.id"))
+    song_id = db.Column(db.Integer, db.ForeignKey("songs.id"))
+
 
 
 # DO NOT MODIFY THIS FUNCTION
